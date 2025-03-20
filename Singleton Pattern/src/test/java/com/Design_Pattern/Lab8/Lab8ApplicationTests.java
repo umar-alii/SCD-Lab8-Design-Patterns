9ccef287f44c85c9fb460d9962c325bebe5c7182
umar-alii <umarrali.pk@gmail.com>
2025-03-20 09:18:36 +0500

    init

---
+++ Singleton Pattern/src/test/java/com/Design_Pattern/Lab8/Lab8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Design_Pattern.Lab8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
